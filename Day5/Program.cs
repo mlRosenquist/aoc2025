bb5e00053d36f43067795917109aaa975ee3d67d
mlRosenquist <lyngrosenquistmorten@gmail.com>
2025-12-07 07:10:58 +0100

    Updated Day5

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,112 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections.Immutable;
 
-
-using System.Collections.Immutable;
-
-////var amountFreshIngredients = 6;
-////PartOne();
+var amountFreshIngredients = 194;
+PartOne();
 PartTwo();
-////void PartOne()
-////{
-////    var input = File.ReadAllText("input.txt");
-////    var fresh = FreshIngredients(input);
-////    var available = AvailableIngredients(input);
-
-////    var amountFresh = available
-////        .Count(IsFresh);
-    
-////    Console.WriteLine(amountFresh);
-////    bool IsFresh(long ingredient) =>
-////        fresh.Any(x => x.Start <= ingredient && ingredient <= x.End);
-////}
+void PartOne()
+{
+    var input = File.ReadAllText("input.txt");
+    var fresh = FreshIngredients(input);
+    var available = AvailableIngredients(input);
+    var amountFresh = available
+        .Count(IsFresh);
+        Console.WriteLine(amountFresh);
+    bool IsFresh(long ingredient) =>
+        fresh.Any(x => x.Start <= ingredient && ingredient <= x.End);
+}
 
 void PartTwo()
 {
     var input = File.ReadAllText("input.txt");
     var fresh = FreshIngredients(input);
-
-    var handledRanges = new List<(long Start, long End)>();
-
-    foreach ((var start, var end) in fresh)
-    {
-        var completelyOverlappingRange = handledRanges.Where(x => x.Start <= start && x.End >= end);
-        if (completelyOverlappingRange.Any())
-        {
-            continue;
-        }
-
-        var lowerOverlappingRanges = handledRanges
-            .Where(x => x.Start <= start && x.End <= end)
-            .ToImmutableArray();
-        var higherOverlappingRanges = handledRanges
-            .Where(x => x.Start >= start && x.End >= end)
-            .ToImmutableArray();
-
-        var coveringRange = (start, end);
-
-        if (lowerOverlappingRanges.Any())
-        {
-            var min = lowerOverlappingRanges.Min(x => x.Start);
-            coveringRange.start = min;
-        }
-
-        if (higherOverlappingRanges.Any())
-        {
-            var max = higherOverlappingRanges.Max(x => x.End);
-            coveringRange.end = max;
-        }
-
-        if (start < coveringRange.start)
-        {
-            coveringRange.start = start;
-        }
-
-        if (end > coveringRange.end)
-        {
-            coveringRange.end = end;
-        }
-
-        foreach (var oneOfOverlappingRange in handledRanges.Where(x => x.Start == start + 1).ToImmutableArray())
-        {
-            handledRanges.Add((start, oneOfOverlappingRange.End));
-        }
-
-        foreach (var oneOfOverlappingRange in handledRanges.Where(x => x.End == start - 1).ToImmutableArray())
-        {
-            handledRanges.Add((oneOfOverlappingRange.Start, end));
-        }
-
-        handledRanges.Add(coveringRange);
-        foreach (var range in handledRanges.ToImmutableArray())
-        {
-            if (handledRanges.Any(x => 
-                    x.Start <= range.Start && x.End >= range.End &&
-                    (x.Start != range.Start || x.End != range.End)))
-            {
-                handledRanges.Remove(range);
-            }
-        }
-        Console.WriteLine($"({start},{end}):");
-        foreach (var range in handledRanges)
-        {
-            Console.WriteLine($"({range.Start},{range.End})");
-        }
-        Console.WriteLine($"---");
-    }
-
-    var result = handledRanges
+    var uniqueRanges = GetUniqueRanges(fresh);
+    var result = uniqueRanges
         .Sum(x => x.End - x.Start + 1);
     Console.WriteLine(result);
 }
 
-ImmutableArray<(long Start, long End)> FreshIngredients(string input) =>
-    input.Split("\r\n")//[..amountFreshIngredients]
-        .Select(x => x.Split("-"))
-        .Select(x => (long.Parse(x[0].Trim()), long.Parse(x[1].Trim())))
-        .ToImmutableArray();
+ImmutableArray<Range> FreshIngredients(string input) =>
+    [
+        ..input.Split("\r\n")[..amountFreshIngredients]
+            .Select(x => x.Split("-"))
+            .Select(x => new Range(long.Parse(x[0].Trim()), long.Parse(x[1].Trim())))
+    ];
 
-////ImmutableArray<long> AvailableIngredients(string input) =>
-////    [
-////        ..input.Split("\r\n")[(amountFreshIngredients + 1)..]
-////            .Select(long.Parse)
-////    ];
+ImmutableArray<long> AvailableIngredients(string input) =>
+[
+    ..input.Split("\r\n")[(amountFreshIngredients + 1)..]
+        .Select(long.Parse)
+];
+
+HashSet<Range> GetUniqueRanges(ImmutableArray<Range> immutableArray)
+{
+    var hashSet = new HashSet<Range>();
+    foreach (var range in immutableArray)
+    {
+        var lowerOverlappingRanges = hashSet
+            .Where(x => 
+                (x.Start <= range.Start && x.End <= range.End && x.End >= range.Start) ||
+                x.End == range.Start - 1)
+            .ToImmutableArray();
+        var higherOverlappingRanges = hashSet
+            .Where(x => 
+                (x.Start >= range.Start && x.End >= range.End && x.Start <= range.End) ||
+                x.Start == range.End + 1)
+            .ToImmutableArray();
+
+        var newRange = (range.Start, range.End);
+        if (lowerOverlappingRanges.Any())
+        {
+            var min = lowerOverlappingRanges.Min(x => x.Start);
+            newRange.Start = min;
+        }
+        if (higherOverlappingRanges.Any())
+        {
+            var max = higherOverlappingRanges.Max(x => x.End);
+            newRange.End = max;
+        }
+
+        hashSet.Add(new(newRange.Start, newRange.End));
+        Trim();
+    }
+
+    return hashSet;
+
+    void Trim()
+    {
+        foreach (var current in hashSet.ToImmutableArray())
+        {
+            if (hashSet.Any(x => 
+                    x.Start <= current.Start && x.End >= current.End &&
+                    (x.Start != current.Start || x.End != current.End)))
+            {
+                hashSet.Remove(current);
+            }
+        }
+    }
+}
+
+record Range(long Start, long End);
